b2f743497c1e9f3f4dfbde09db0429b2c270aa46
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-11-17 16:50:33 -0400

    nueva consulta para extraer los miembros del grupo developers dominicanos

--- src/main/java/org/devdom/fbclient/FQL.java
+++ src/main/java/org/devdom/fbclient/FQL.java
@@ -5,7 +5,7 @@ package org.devdom.fbclient;
  * @author Carlos Vásquez Polanco
  */
 final public class FQL {
-    
+
     public static final String QUESTION_OPTIONS = "SELECT id, question_id, name, votes from question_option where question_id = :question_id order by votes desc";
     public static final String QUESTION_OPTION_VOTES = "select voter_id, option_id from question_option_votes where option_id = :option_id";
     public static final String GROUP_USERS = "select uid, first_name, last_name, name, pic_small, pic_big, pic from user where uid in (select uid from group_member where gid = :group_id)";
