b81fcd0aac6358d631d92b03e292d06e5ff3b973
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-11-18 21:36:47 -0400

    creando clase para llamada del cliente

---
+++ src/main/java/org/devdom/fbclient/App.java
@@ -0,0 +1,21 @@
+package org.devdom.fbclient;
+
+/**
+ *
+ * @author Carlos Vasquez Polanco
+ */
+public class App 
+{
+    
+    public static void main( String[] args )
+    {
+        FBConnect fbConnect = new FBConnect();
+        fbConnect.setGroupID("201514949865358");
+        fbConnect.setQuestionID("323442071005978"); //Cuales son tus skillls?
+        fbConnect.updateSkills();
+        fbConnect.setQuestionID("562574913759358"); //Cuales son tus skillls? (Plataforma para la que desarrollas)
+        fbConnect.updateSkills();
+        fbConnect.setQuestionID("558308817519301"); //Cuales son tus skillls? (Manejadores de versión)
+        fbConnect.updateDevelopers();
+    }
+}
